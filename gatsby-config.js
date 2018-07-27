d501c75d071caf7801fb37e706bf9bc772a412f5
seonim-ryu <seonim.ryu@nhnent.com>
2018-07-27 15:02:50 +0900

    feat: apply sass (close #3)

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data`,
+      },
+    }
+  ],
+}

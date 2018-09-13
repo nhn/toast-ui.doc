2fb36846d06851805a149a22cf6b54c9f74ac6e0
seonim-ryu <seonim.ryu@nhnent.com>
2018-09-13 09:51:03 +0900

    fix: edit path-prefix on node config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,5 @@
-const path = require('path');
-const pwd = process.cwd();
-const options = require(path.resolve(pwd, 'src/data/layout.json'));
-
-const {header, fileLink} = options[0];
-const {version} = header;
-const repoName = fileLink.repository.split('/').pop().replace('.git', '');
-
 module.exports = {
-  pathPrefix: `/${repoName}/${version}`,
+  pathPrefix: `./`,
   plugins: [
     `gatsby-plugin-sass`,
     {
